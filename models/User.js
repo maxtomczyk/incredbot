eb396c7e0d04cba4ac35852b1bf66a1b6305f0e5
Maksymilian Tomczyk <m.tomczyk@incredbots.com>
2018-10-05 19:00:42 +0200

    Changed user data fetching function. Now it's accepts fields list

--- models/User.js
+++ models/User.js
@@ -6,36 +6,37 @@ const logger = require('eazy-logger').Logger({
     useLevelPrefixes: true
 })
 
-
 class User {
     constructor(messenger_id, config, sender) {
-      if(!messenger_id) logger.warn('You must pass messenger id of user!')
-      this.config = config || {}
-      this.messenger_id = messenger_id
-      this.api_url = `https://graph.facebook.com/${config.api_version}/${this.messenger_id}?fields=first_name,last_name,profile_pic&access_token=${config.access_token}`
-      this.send = sender
-      this.botanalytics = (this.config.botanalytics) ? Botanalytics.FacebookMessenger(this.config.botanalytics.token) : false
+        if (!messenger_id) logger.warn('You must pass messenger id of user!')
+        this.config = config || {}
+        this.messenger_id = messenger_id
+        this.api_url = `https://graph.facebook.com/${config.api_version}/${this.messenger_id}`
+        this.send = sender
+        this.botanalytics = (this.config.botanalytics) ? Botanalytics.FacebookMessenger(this.config.botanalytics.token) : false
     }
 
-    async getData(){
+    async getData(...fields) {
         try {
-          let data = await axios.get(this.api_url)
+            fields = (fields.length > 0) ? fields : ['first_name, last_name, id, locale, timezone, gender']
+            let url = `${this.api_url}?fields=${fields.toString().replace(' ', '')}&access_token=${this.config.access_token}`
+            let data = await axios.get(url)
 
-          if(this.botanalytics){
-            let analyticsData = Object.assign({}, data.data)
-            analyticsData.user_id = analyticsData.id
+            if (this.botanalytics) {
+                let analyticsData = Object.assign({}, data.data)
+                analyticsData.user_id = analyticsData.id
 
-            delete analyticsData.last_ad_referral
-            delete analyticsData.is_payment_enabled
-            delete analyticsData.id
+                delete analyticsData.last_ad_referral
+                delete analyticsData.is_payment_enabled
+                delete analyticsData.id
 
-            this.botanalytics.logUserProfile(analyticsData)
-          }
+                this.botanalytics.logUserProfile(analyticsData)
+            }
 
-          return data.data
+            return data.data
         } catch (e) {
-          console.error(e.response.data);
-          logger.error(e)
+            console.error(e.response.data);
+            logger.error(e)
         }
     }
 }

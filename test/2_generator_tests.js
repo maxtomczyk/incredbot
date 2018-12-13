026e3e8a477378f239902a01daeecc6ffb6fb561
Maksymimlian Tomczyk <m.tomczyk.dev@gmail.com>
2018-12-13 21:16:32 +0100

    Added Frame class tests

--- test/2_generator_tests.js
+++ test/2_generator_tests.js
@@ -82,3 +82,93 @@ describe('Text Generator', function () {
         expect(m).to.deep.equal(correct)
     })
 })
+
+describe('Frame Class', function () {
+    it('Should wrap message with recipient as parameter', () => {
+        const btns = [new incredbot.Helpers.Button('web_url', 'Button', 'https://google.com')]
+        const m = new incredbot.Message.Buttons('Testing...', btns)
+        const framed = new incredbot.Frame(m, config.testUserId, {
+            messaging_type: 'MESSAGE_TAG'
+        })
+
+        const correct = {
+            message: {
+                attachment: {
+                    payload: {
+                        buttons: [{
+                            title: 'Button',
+                            type: 'web_url',
+                            url: 'https://google.com'
+                        }],
+                        template_type: 'button',
+                        text: 'Testing...'
+                    },
+                    type: 'template'
+                }
+            },
+            messaging_type: 'MESSAGE_TAG',
+            recipient: {
+                id: config.testUserId
+            }
+        }
+        expect(framed).to.deep.equal(correct)
+    })
+    it('Should wrap message with options object only', () => {
+        const btns = [new incredbot.Helpers.Button('web_url', 'Button', 'https://google.com')]
+        const m = new incredbot.Message.Buttons('Testing...', btns)
+        const framed = new incredbot.Frame(m, {
+            messaging_type: 'MESSAGE_TAG',
+            recipient_id: config.testUserId
+        })
+
+        const correct = {
+            message: {
+                attachment: {
+                    payload: {
+                        buttons: [{
+                            title: 'Button',
+                            type: 'web_url',
+                            url: 'https://google.com'
+                        }],
+                        template_type: 'button',
+                        text: 'Testing...'
+                    },
+                    type: 'template'
+                }
+            },
+            messaging_type: 'MESSAGE_TAG',
+            recipient: {
+                id: config.testUserId
+            }
+        }
+        expect(framed).to.deep.equal(correct)
+    })
+
+    it('Should wrap message without options object', () => {
+        const btns = [new incredbot.Helpers.Button('web_url', 'Button', 'https://google.com')]
+        const m = new incredbot.Message.Buttons('Testing...', btns)
+        const framed = new incredbot.Frame(m, config.testUserId)
+
+        const correct = {
+            message: {
+                attachment: {
+                    payload: {
+                        buttons: [{
+                            title: 'Button',
+                            type: 'web_url',
+                            url: 'https://google.com'
+                        }],
+                        template_type: 'button',
+                        text: 'Testing...'
+                    },
+                    type: 'template'
+                }
+            },
+            messaging_type: 'RESPONSE',
+            recipient: {
+                id: config.testUserId
+            }
+        }
+        expect(framed).to.deep.equal(correct)
+    })
+})
